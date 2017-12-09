03b2c2aa97fb144d73d8a1776295964451b01513
zeroxys <miguelzavalac@gmail.com>
2017-12-09 10:42:14 -0600

    fist commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import './css/App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>miguelzavala.tech</h1>
+          <nav>
+            <ul>
+              <li><a href="#">enlace 1</a></li>
+              <li><a href="#">enlace 1</a></li>
+              <li><a href="#">enlace 1</a></li>
+            </ul>
+          </nav>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App
