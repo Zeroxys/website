7c69e9a00b08a044d9c2d290d308394fa85e71c6
Miguel Zavala <miguelzavalac@gmail.com>
2024-03-20 10:28:58 -0600

    .

--- src/components/pdf/Pdf.tsx
+++ src/components/pdf/Pdf.tsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import { Document, Page, PDFDownloadLink } from '@react-pdf/renderer';
-
-// Establecer el worker de PDF.js para evitar errores en el navegador
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const PdfDocument = () => {
   return (
